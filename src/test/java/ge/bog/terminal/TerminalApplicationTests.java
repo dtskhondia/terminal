db1f61ff6f89831a02a566a4afe04cf226b03935
dtskhondia <datotskhondia@gmail.com>
2024-10-14 17:34:30 +0400

    init project

---
+++ src/test/java/ge/bog/terminal/TerminalApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.bog.terminal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerminalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
